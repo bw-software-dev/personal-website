5fe962b114e5f65c9fdb1c16eb0ad36f5e7a405a
bwright <benwboston@gmail.com>
2025-04-19 23:47:16 +0000

    Move homepage to root path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,13 +20,10 @@
 </script>
 
 <nav
-  class="flex items-center space-x-5 border-b-1 border-b-stone-300 h-15 px-2 text-xl text-white dark:border-b-stone-800 dark:bg-stone-950"
+  class="flex h-15 items-center space-x-5 border-b-1 border-b-stone-300 px-2 text-xl text-white dark:border-b-stone-800 dark:bg-stone-950"
 >
   <img src="logo.svg" alt="logo" class="size-10" />
-  <a
-    href="/home"
-    class={["px-1 underline-offset-2", route == "/home" && "underline"]}
-  >
+  <a href="/" class={["px-1 underline-offset-2", route == "/" && "underline"]}>
     Home
   </a>
   <a
@@ -44,10 +41,10 @@
 </nav>
 
 <div
-  class="fixed flex justify-center inset-0 top-15 overflow-y-scroll bg-white dark:bg-linear-0 dark:from-black dark:to-stone-900"
+  class="fixed inset-0 top-15 flex justify-center overflow-y-scroll bg-white dark:bg-linear-0 dark:from-black dark:to-stone-900"
 >
   <article
-    class="md:m-10 m-0 prose md:min-w-250 md:rounded-md md:border-1 md:border-stone-300 md:bg-white p-10 md:dark:border-stone-800 dark:bg-stone-950 dark:prose-invert"
+    class="m-0 prose p-10 md:m-10 md:min-w-250 md:rounded-md md:border-1 md:border-stone-300 md:bg-white dark:bg-stone-950 dark:prose-invert md:dark:border-stone-800"
   >
     {@render children()}
   </article>
